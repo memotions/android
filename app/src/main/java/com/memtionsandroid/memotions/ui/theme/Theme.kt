e37d38a3d203d8ddc831de314e392996d2b0a325
LianggaRistiana <liangga4@gmail.com>
2024-11-22 03:21:35 +0800

    feat: define custom color palette for the app theme

--- app/src/main/java/com/memtionsandroid/memotions/ui/theme/Theme.kt
+++ app/src/main/java/com/memtionsandroid/memotions/ui/theme/Theme.kt
@@ -1,15 +1,11 @@
 package com.memtionsandroid.memotions.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -21,17 +17,47 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
+
+data class CustomColors(
+    val barColor: Color,
+    val onBarColor: Color,
+    val onBarSecondColor: Color,
+    val outlineColor: Color,
+    val processColor: Color,
+    val processBackgroundColor: Color,
+    val backgroundColor: Color,
+    val secondBackgroundColor: Color,
+    val onBackgroundColor: Color,
+    val onSecondBackgroundColor: Color
+)
+
+private val LightCustomColor = CustomColors(
+    barColor = Charcoal,
+    onBarColor = PureWhite,
+    onBarSecondColor = LightGrey,
+    outlineColor = Taupe,
+    processColor = Charcoal,
+    processBackgroundColor = Silver,
+    backgroundColor = OffWhite,
+    secondBackgroundColor = Ivory,
+    onBackgroundColor = Charcoal,
+    onSecondBackgroundColor = SteelGrey,
+)
+
+private val DarkCustomColor = CustomColors(
+    barColor = Charcoal,
+    onBarColor = PureWhite,
+    onBarSecondColor = LightGrey,
+    outlineColor = Taupe,
+    processColor = LightGrey,
+    processBackgroundColor = OnyxGrey,
+    backgroundColor = SlateBLue,
+    secondBackgroundColor = OnyxGrey,
+    onBackgroundColor = PureWhite,
+    onSecondBackgroundColor = LightGrey,
+)
+
 
 @Composable
 fun MemotionsTheme(
@@ -40,11 +66,12 @@ fun MemotionsTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val customColor = if (darkTheme) DarkCustomColor else LightCustomColor
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
