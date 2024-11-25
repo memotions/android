d8e1a41589008e9fb4bf6943392a6eb31d534c40
LianggaRistiana <liangga4@gmail.com>
2024-11-25 12:10:40 +0800

    feat: add profile screen with topbar

---
+++ app/src/main/java/com/memtionsandroid/memotions/ui/components/profile/PersonalInfo.kt
@@ -0,0 +1,57 @@
+package com.memtionsandroid.memotions.ui.components.profile
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.memtionsandroid.memotions.R
+import com.memtionsandroid.memotions.ui.theme.customColors
+
+
+data class User(
+    val name: String,
+    val yearCreated: String,
+)
+
+@Composable
+fun PersonalInfo(user: User) {
+    val customColors = MaterialTheme.customColors
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "Profile Picture",
+            modifier = Modifier.size(40.dp)
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 8.dp)
+        ) {
+            Text(
+                text = user.name,
+                style = MaterialTheme.typography.titleMedium,
+                color = customColors.onBackgroundColor
+            )
+            Text(
+                text = "Bergabung sejak ${user.yearCreated}",
+                style = MaterialTheme.typography.bodySmall,
+                color = customColors.onSecondBackgroundColor
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PersonalInfoPreview() {
+    val user = User(name = "Liangga", yearCreated = "2024")
+    PersonalInfo(user)
+}
--- app/src/main/java/com/memtionsandroid/memotions/ui/main/screen/ProfileScreen.kt
+++ app/src/main/java/com/memtionsandroid/memotions/ui/main/screen/ProfileScreen.kt
@@ -2,13 +2,24 @@ package com.memtionsandroid.memotions.ui.main.screen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.memtionsandroid.memotions.ui.components.main.Journals
+import com.memtionsandroid.memotions.ui.components.profile.ProfileTopBar
+import com.memtionsandroid.memotions.ui.components.starred.StarredTopBar
 
 @Composable
 fun ProfileScreen() {
-    Box (Modifier.fillMaxSize()){
-        Text("Profile Screen")
-    }
+    Scaffold (
+        topBar = {
+            ProfileTopBar()
+        },
+        content = { innerPadding ->
+        }
+    )
 }
